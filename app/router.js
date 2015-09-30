5957337c63057cf228ec5c03fe6f275041b3c981
NguyenQuiDuong <duongnqse02934@fpt.edu.vn>
2015-09-30 23:01:49 +0700

    Initial commit

---
+++ app/router.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('user', function() {
+    this.route('signin');
+    this.route('signup');
+  });
+});
+
+export default Router;
